b453aa3f4ef2e5bab1abc2d10835267017ec0f2e
李建刚 <holine@holinedeMacBook-Pro.local>
2020-09-08 17:13:06 +0800

    修复client_ip的bug

--- src/Autoloader.php
+++ src/Autoloader.php
@@ -2,17 +2,11 @@
 
 namespace MicroPHP;
 
-spl_autoload_register(fn ($class) => autoload($class));
-
-function autoload(String $class, String $namespace = __NAMESPACE__, String $path = __DIR__)
-{
-    if (strpos($class, $namespace . '\\') === 0) {
-        $path = $path . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, substr($class, strlen($namespace) + 1)) . '.php';
-        if (is_readable($path)) {
-            require $path;
-        }
-    }
+foreach (glob(__DIR__ . '/Functions/*.php') as $file) {
+    include $file;
 }
+
+spl_autoload_register(fn ($class) => autoload($class));
 
 class Autoloader
 {
--- src/Functions/autoload.php
+++ src/Functions/autoload.php
@@ -2,40 +2,13 @@
 
 namespace MicroPHP;
 
-function autoload(String $class, String $namespace = __NAMESPACE__, String $path = __DIR__)
+function autoload(String $class, String $namespace = __NAMESPACE__, String $path = null)
 {
+    $path ??= dirname(__DIR__);
     if (strpos($class, $namespace . '\\') === 0) {
         $path = $path . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, substr($class, strlen($namespace) + 1)) . '.php';
         if (is_readable($path)) {
             require $path;
         }
     }
-}
-
-function client_ip()
-{
-    $realip = '0.0.0.0';
-    if (isset($_SERVER)) {
-        if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $arr = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-            foreach ($arr as $ip) {
-                $ip = trim($ip);
-                if ($ip != 'unknown') {
-                    $realip = $ip;
-                    break;
-                }
-            }
-        } else if (isset($_SERVER['HTTP_CLIENT_IP'])) {
-            $realip = $_SERVER['HTTP_CLIENT_IP'];
-        } else if (isset($_SERVER['REMOTE_ADDR'])) {
-            $realip = $_SERVER['REMOTE_ADDR'];
-        }
-    } else if (getenv('HTTP_X_FORWARDED_FOR')) {
-        $realip = getenv('HTTP_X_FORWARDED_FOR');
-    } else if (getenv('HTTP_CLIENT_IP')) {
-        $realip = getenv('HTTP_CLIENT_IP');
-    } else {
-        $realip = getenv('REMOTE_ADDR');
-    }
-    return $realip;
 }
