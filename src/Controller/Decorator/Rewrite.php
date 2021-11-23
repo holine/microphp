7e4c99625187cf8044ef2f746d67fcdd10bd3d38
holine <development@holine.studio>
2021-11-24 00:13:10 +0800

    修复无rewrite规则时的bug

--- src/Controller/Decorator/Rewrite.php
+++ src/Controller/Decorator/Rewrite.php
@@ -11,7 +11,7 @@ class Rewrite extends Decorator
 {
     public function execute(Request $request, Response $response)
     {
-        foreach (Configure::read('decorator.rewrite') as $pattern => $rules) {
+        foreach (Configure::read('decorator.rewrite') ?? [] as $pattern => $rules) {
             if (preg_match_all($pattern, $request->server('REQUEST_URI'), $match)) {
                 $uri = $rules['router'] + Configure::read('decorator.router.default');
                 $request->server('REQUEST_URI', "{$uri['controller']}/{$uri['action']}");
