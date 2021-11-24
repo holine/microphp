b0d35110ad147d091b01737420e373ae737f4083
holine <development@holine.studio>
2021-11-24 10:45:59 +0800

    支持unix_socket

--- src/Model/MySQL.php
+++ src/Model/MySQL.php
@@ -4,9 +4,9 @@ namespace MicroPHP\Model;
 
 class MySQL
 {
-    static public function dsn(array $config = ['host' => 'localhost', 'database' => 'test', 'port' => 3306]): string
+    static public function dsn(array $config = ['host' => 'localhost', 'database' => 'test', 'port' => 3306, 'unix_socket' => null,]): string
     {
         $config['port'] ??= 3306;
-        return "mysql:dbname={$config['database']};host={$config['host']};port={$config['port']};charset=UTF8";
+        return $config['unix_socket'] ? "mysql:dbname={$config['database']};unix_socket={$config['unix_socket']};charset=UTF8" : "mysql:dbname={$config['database']};host={$config['host']};port={$config['port']};charset=UTF8";
     }
 }
