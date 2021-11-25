c5cc2eef6406653ed689596274175956bd796c09
holine <development@holine.studio>
2021-11-25 17:32:45 +0800

    判断是否设置socket

--- src/Model/MySQL.php
+++ src/Model/MySQL.php
@@ -7,6 +7,6 @@ class MySQL
     static public function dsn(array $config = ['host' => 'localhost', 'database' => 'test', 'port' => 3306, 'unix_socket' => null,]): string
     {
         $config['port'] ??= 3306;
-        return $config['unix_socket'] ? "mysql:dbname={$config['database']};unix_socket={$config['unix_socket']};charset=UTF8" : "mysql:dbname={$config['database']};host={$config['host']};port={$config['port']};charset=UTF8";
+        return isset($config['unix_socket']) ? "mysql:dbname={$config['database']};unix_socket={$config['unix_socket']};charset=UTF8" : "mysql:dbname={$config['database']};host={$config['host']};port={$config['port']};charset=UTF8";
     }
 }
